6dbc246012a871bba9dae6e2579008a7052ebe4b
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-11-26 17:42:48 +0100

    starter template, copied all html and css code from github

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
 
-function ExampleComponent() {
+function Main() {
   return (
-    <div>
-      <h1>This is our app!</h1>
-      <p>The sky is blue</p>
-    </div>
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
   );
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector("#app"));
+ReactDOM.render(<Main />, document.querySelector("#app"));
 if (module.hot) {
   module.hot.accept();
 }
