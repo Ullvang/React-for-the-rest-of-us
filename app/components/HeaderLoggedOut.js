8d82ff46128567329b965bf1a9da29d919a5810f
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-11-30 18:51:28 +0100

    Logging in

---
+++ app/components/HeaderLoggedOut.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+export default function HeaderLoggedOut() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await Axios.post("http://localhost:8080/login", {
+        username,
+        password,
+      });
+      if (response.data) {
+        console.log(response.data);
+      } else {
+        console.log("Incorrect login info");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form className="mb-0 pt-2 pt-md-0" onSubmit={handleSubmit}>
+      <div className="row align-items-center">
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input
+            name="username"
+            className="form-control form-control-sm input-dark"
+            type="text"
+            placeholder="Username"
+            autoComplete="off"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input
+            name="password"
+            className="form-control form-control-sm input-dark"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="col-md-auto">
+          <button className="btn btn-success btn-sm">Sign In</button>
+        </div>
+      </div>
+    </form>
+  );
+}
