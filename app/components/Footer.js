c281a9fe97c64c9739894c07426c84d51838015d
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-12-01 22:59:02 +0100

    conditional homepage content

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -28,4 +28,4 @@ function Footer() {
   );
 }
 
-export default Footer;
+export default React.memo(Footer);
--- app/components/Header.js
+++ app/components/Header.js
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(
-    Boolean(localStorage.getItem("complexappToken"))
-  );
-
+function Header({ loggedIn, setLoggedIn }) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
