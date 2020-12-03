785d09b1841112da0ab68bbb3586ffd83b13ea7a
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-12-03 19:44:42 +0100

    load posts by author

---
+++ app/components/ProfilePosts.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { useParams, Link } from "react-router-dom";
+
+export default function ProfilePosts() {
+  const { username } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/posts`);
+        setPosts(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log("There was a problem");
+        console.log(e);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="list-group">
+      {posts.map((post) => {
+        const date = new Date(post.createdDate);
+        const dateFormatted = `${date.getDate()}/${
+          date.getMonth() + 1
+        }/${date.getFullYear()}`;
+
+        return (
+          <Link
+            key={post._id}
+            to={`/post/${post._id}`}
+            className="list-group-item list-group-item-action"
+          >
+            <img className="avatar-tiny" src={post.author.avatar} />{" "}
+            <strong>{post.title} </strong>
+            <span className="text-muted small">on {dateFormatted}</span>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
